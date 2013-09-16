81ffa81b2b03673e259a8e5ad5da85a40eb74c87
Joe Hirn <joseph.hirn@gmail.com>
2013-09-15 23:21:20 -0500

    Updates to debian install scripts.

--- debian-vm/rbenv.sh
+++ debian-vm/rbenv.sh
@@ -3,12 +3,19 @@
 
 #Ruby
 mkdir -p ~/src/ruby
-git clone https://github.com/fesplugas/rbenv-installer ~/src/ruby
+
+if [ ! -d ~/src/ruby/rbenv-installer ]; then
+    git clone https://github.com/fesplugas/rbenv-installer ~/src/ruby/rbenv-installer
+fi
 
 chmod +x ~/src/ruby/rbenv-installer/bin
-~/src/ruby/rbenv-installer/bin
-git clone https://github.com/ianheggie/rbenv-binstubs ~/.rbenv/plugins
-bash -l
+~/src/ruby/rbenv-installer/bin/rbenv-installer
+
+if [ ! -d ~/.rbenv/plugins/rbenv-binstubs ]; then
+    git clone https://github.com/ianheggie/rbenv-binstubs ~/.rbenv/plugins/rbenv-binstubs
+fi
+PATH=$HOME/.rbenv/bin:$PATH
+eval "$(rbenv init - --no-rehash)"
 rbenv install 2.0.0-p247 
 
 
