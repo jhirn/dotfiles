d959224f1dcd10acd612fb93b71d71cb82d1b0a2
Joe Hirn <joseph.hirn@gmail.com>
2019-06-27 15:24:38 -0500

    Fix git global git ignores.

--- install.rb
+++ install.rb
@@ -3,7 +3,7 @@
 home = File.expand_path('~')
 
 Dir['*'].each do |file|
-  next if file =~ /install/
+  next if file =~ /install|Brewfile|README/
   target = File.join(home, ".#{file}")
   `ln -s #{File.expand_path file} #{target}` unless File.exists?(target)
 end
