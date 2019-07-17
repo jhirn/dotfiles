a9211fa42fa9b060d2e49d6d9b601a3bd3db3c31
Joe Hirn <joseph.hirn@gmail.com>
2019-07-17 17:46:02 -0500

    Asdf > rbenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,13 @@
 set -x fish_user_paths $fish_user_paths /usr/local/sbin
 
 set -x EDITOR "emacs -Q"
+set -x ASDF_DIR (brew --prefix asdf)
 
-set -gx RBENV_ROOT ~/.rbenv
-source (rbenv init -|psub)
+#set -gx RBENV_ROOT ~/.rbenv
+#source (rbenv init -|psub)
 
 source ~/.aliases
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+source (brew --prefix asdf)/asdf.fish
