c921a69b88d3ad682a2a01b956bad5faa33161ec
Joe Hirn <joseph.hirn@gmail.com>
2020-10-07 21:14:55 -0500

    No brew prefix

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,16 +4,16 @@ set -g fish_user_paths /usr/local/bin $fish_user_paths
 set -x fish_user_paths $fish_user_paths /usr/local/sbin
 set -gx PKG_CONFIG_PATH /usr/local/lib/pkgconfig
 
-set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=(brew --prefix openssl@1.1)
+set -gx RUBY_CONFIGURE_OPTS --with-openssl-dir=/usr/local/opt/openssl@1.1 #(brew --prefix openssl@1.1)
 
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/openjdk-13.0.2.jdk/Contents/Home
 # (/usr/libexec/java_home -v 13)
 
 set -x EDITOR "emacs -Q"
-set -x ASDF_DIR (brew --prefix asdf)
+set -x ASDF_DIR /usr/local/opt/asdf #(brew --prefix asdf)
 
 source ~/.aliases
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-source (brew --prefix asdf)/asdf.fish
+source /usr/local/opt/asdf/asdf.fish
 set -g fish_user_paths "/usr/local/opt/postgresql@11/bin" $fish_user_paths
