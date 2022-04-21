85d60fa0e5a74e933dccb3505f665c9d00220331
Joseph Hirn <joseph.hirn@gmail.com>
2022-04-21 17:49:22 -0500

    Rename tf ip fetchign files. Update to get private ip.

--- config/fish/functions/tf_instances_ec2.fish
+++ config/fish/functions/tf_instances_ec2.fish
@@ -1,4 +1,4 @@
-function tf_public_ips_ec2
+function tf_instances_ec2
   aws ec2 describe-instances \
     --filters Name=instance-state-name,Values=running Name=tag-value,Values="*?" \
     --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress, Server:Tags[?Key=='Name']|[0].Value}" \
