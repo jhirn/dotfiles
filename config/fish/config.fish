456b9dacf3e73e5147b2d3a245502b943e35b199
Joseph Hirn <joseph.hirn@gmail.com>
2022-05-11 09:40:21 -0500

    Fish completion for aws

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -29,3 +29,4 @@ fnm env --use-on-cd | source
 # set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
 
 fish_add_path /opt/homebrew/opt/openjdk/bin
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
